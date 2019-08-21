0a8ebb3d50ca5a29cb2c99141c0ce817160d5f7d
aterlecka <aleksandra.terlecka1996@gmail.com>
2019-08-21 22:43:17 +0200

    improve coffee machine simulation

--- coffeMachine.js
+++ coffeMachine.js
@@ -56,63 +56,99 @@ class ContentsOfTheCoffeeMachine {
 
 }
 
-let contentsOfTheCoffeeMachine = new ContentsOfTheCoffeeMachine(1200, 540, 120, 9, 550);
 
-displayCurrentContentOfMachine();
+let contentsOfTheCoffeeMachine = new ContentsOfTheCoffeeMachine(400, 540, 120, 9, 550);
 
-readline.question('Write action (buy, fill, take): ', (userInput) => {
-    if (userInput === "buy") {
-        buy();
-    }
-    if (userInput === "fill") {
-        fill();
-    }
-    if (userInput === "take") {
-        take();
-    }
-});
+chooseAction();
 
 function buy() {
-    readline.question(' What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ', (coffeeType) => {
+    readline.question('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:', (coffeeType) => {
 
         if (parseInt(coffeeType) === 1) {
             makeEspresso(contentsOfTheCoffeeMachine);
         }
         if (parseInt(coffeeType) === 2) {
             makeLatte(contentsOfTheCoffeeMachine);
+
         }
         if (parseInt(coffeeType) === 3) {
             makeCappuccino(contentsOfTheCoffeeMachine);
         }
+        if (coffeeType === "back") {
+            chooseAction();
+        }
 
         function makeEspresso(contentsOfTheCoffeeMachine) {
+
+            if (contentsOfTheCoffeeMachine.water >= 250 && contentsOfTheCoffeeMachine.beans >= 16 &&
+                contentsOfTheCoffeeMachine.cups >= 1) {
+                console.log("I have enough resources, making you a coffee!")
+            } else if (contentsOfTheCoffeeMachine.water < 250) {
+                console.log("Sorry, not enough water!")
+            } else if (contentsOfTheCoffeeMachine.milk < 0) {
+                console.log("Sorry, not enough milk!")
+            } else if (contentsOfTheCoffeeMachine.beans < 16) {
+                console.log("Sorry, not enough beans!")
+            } else if (contentsOfTheCoffeeMachine.cups < 1) {
+                console.log("Sorry, not enough cups!")
+            }
+
             contentsOfTheCoffeeMachine.water = contentsOfTheCoffeeMachine.water - 250;
             contentsOfTheCoffeeMachine.milk = contentsOfTheCoffeeMachine.milk - 0;
             contentsOfTheCoffeeMachine.beans = contentsOfTheCoffeeMachine.beans - 16;
             contentsOfTheCoffeeMachine.cups = contentsOfTheCoffeeMachine.cups - 1;
-            contentsOfTheCoffeeMachine.money = contentsOfTheCoffeeMachine.money - 4;
+            contentsOfTheCoffeeMachine.money = contentsOfTheCoffeeMachine.money + 4;
+            chooseAction();
 
-            displayCurrentContentOfMachine();
         }
 
+
         function makeLatte(contentsOfTheCoffeeMachine) {
+
+            if (contentsOfTheCoffeeMachine.water >= 350 && contentsOfTheCoffeeMachine.milk >= 75 && contentsOfTheCoffeeMachine.beans >= 20 &&
+                contentsOfTheCoffeeMachine.cups >= 1) {
+                console.log("I have enough resources, making you a coffee!")
+            } else if (contentsOfTheCoffeeMachine.water < 350) {
+                console.log("Sorry, not enough water!")
+            } else if (contentsOfTheCoffeeMachine.milk < 75) {
+                console.log("Sorry, not enough milk!")
+            } else if (contentsOfTheCoffeeMachine.beans < 20) {
+                console.log("Sorry, not enough beans!")
+            } else if (contentsOfTheCoffeeMachine.cups < 1) {
+                console.log("Sorry, not enough cups!")
+            }
+
             contentsOfTheCoffeeMachine.water = contentsOfTheCoffeeMachine.water - 350;
             contentsOfTheCoffeeMachine.milk = contentsOfTheCoffeeMachine.milk - 75;
             contentsOfTheCoffeeMachine.beans = contentsOfTheCoffeeMachine.beans - 20;
             contentsOfTheCoffeeMachine.cups = contentsOfTheCoffeeMachine.cups - 1;
             contentsOfTheCoffeeMachine.money = contentsOfTheCoffeeMachine.money - 7;
+            chooseAction();
 
-            displayCurrentContentOfMachine();
         }
 
         function makeCappuccino(contentsOfTheCoffeeMachine) {
+
+            if (contentsOfTheCoffeeMachine.water >= 200 && contentsOfTheCoffeeMachine.milk >= 100 && contentsOfTheCoffeeMachine.beans >= 12 &&
+                contentsOfTheCoffeeMachine.cups >= 1) {
+                console.log("I have enough resources, making you a coffee!")
+            } else if (contentsOfTheCoffeeMachine.water < 200) {
+                console.log("Sorry, not enough water!")
+            } else if (contentsOfTheCoffeeMachine.milk < 100) {
+                console.log("Sorry, not enough milk!")
+            } else if (contentsOfTheCoffeeMachine.beans < 12) {
+                console.log("Sorry, not enough beans!")
+            } else if (contentsOfTheCoffeeMachine.cups < 1) {
+                console.log("Sorry, not enough cups!")
+            }
+
             contentsOfTheCoffeeMachine.water = contentsOfTheCoffeeMachine.water - 200;
             contentsOfTheCoffeeMachine.milk = contentsOfTheCoffeeMachine.milk - 100;
             contentsOfTheCoffeeMachine.beans = contentsOfTheCoffeeMachine.beans - 12;
             contentsOfTheCoffeeMachine.cups = contentsOfTheCoffeeMachine.cups - 1;
             contentsOfTheCoffeeMachine.money = contentsOfTheCoffeeMachine.money - 6;
+            chooseAction();
 
-            displayCurrentContentOfMachine();
         }
     });
 }
@@ -133,7 +169,7 @@ function fill() {
                     readline.question('Write how much of money do you want to add: ', (howMuchMoney) => {
                         contentsOfTheCoffeeMachine.money = contentsOfTheCoffeeMachine.money + parseInt(howMuchMoney);
 
-                        displayCurrentContentOfMachine();
+                        chooseAction();
 
                     });
                 });
@@ -142,18 +178,45 @@ function fill() {
     });
 }
 
-function take() {
-    readline.question('I give you: ', (howMuchMoney) => {
-        contentsOfTheCoffeeMachine.money = contentsOfTheCoffeeMachine.money - howMuchMoney;
-        displayCurrentContentOfMachine()
+function chooseAction() {
+    readline.question('Write action (buy, fill, take, remaining, exit): ', (userInput) => {
+        if (userInput === "buy") {
+            buy();
+        }
+        if (userInput === "fill") {
+            fill();
+        }
+        if (userInput === "take") {
+            take();
+        }
+        if (userInput === "remaining") {
+            remaining();
+        }
+        if (userInput === "exit") {
+            exit();
+        }
     });
 }
 
-function displayCurrentContentOfMachine() {
+function exit() {
+    process.exit(0);
+}
+
+function take() {
+    readline.question('I give you: ', (howMuchMoney) => {
+        contentsOfTheCoffeeMachine.money = contentsOfTheCoffeeMachine.money - howMuchMoney;
+        chooseAction();
+    });
+}
+
+function remaining() {
     console.log("The coffee machine has:");
     console.log(contentsOfTheCoffeeMachine.water + " of water");
     console.log(contentsOfTheCoffeeMachine.milk + " of milk");
     console.log(contentsOfTheCoffeeMachine.beans + " of coffee beans");
     console.log(contentsOfTheCoffeeMachine.cups + " of cups");
     console.log(contentsOfTheCoffeeMachine.money + " of money");
+    chooseAction();
 }
+
+
